a6a5843e78c9a363a040abfeb3d5194bfba68c36
Adeniran Solomon <soluwaseun806@gmail.com>
2023-07-12 04:54:20 +0100

    Updated code

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 //setting up the gameboard Module
 let gameBoardModule = (function(){
-    let gameBoard = [];
-    return {};
-})();
+    let gameBoard = ["","","","","","","","",""];
 
-//setting up display controller Module
-let displayController = (function(){
-   let testF = () =>{console.log("hey you")}
-   return {testF};
-})();
-
-//setting up the player factory function
-const createPlayer = (name,mark) => {
-    return {
-        name,
-        mark 
+//render function displays to game to the screen
+    const render=()=>{
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell,index) =>{
+            cell.textContent = gameBoard[index];
+        });
     }
-}
-
-//render function to display array
-let renderArray (function() {
-    const gridBoxes = document.querySelectorsAll(".grid-box");
-    return{};
+    
+    return {render,
+    };
 })();
+
+//factory function to create players
+const createPlayer = (name,mark) => {
+        return {
+            name,
+            mark 
+        };
+    };
+
+//tells the game what to do
+const Game = (() => {
+    let players =[];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start =() =>{
+        players =[
+                  createPlayer(document.querySelector("#player1").value,"X"),
+                  createPlayer(document.querySelector("#player2").value, "0")
+                ]
+                currentPlayerIndex=0;
+                gameOver = false;
+                gameBoardModule.render();
+    };
+
+    const handleClick = (event) =>{
+        console.log(event)
+    }
+    return{
+        start,handleClick
+    };
+})();
+document.addEventListener("DOMContentLoaded", () => {
+    const startButton = document.querySelector("#start-button");
+    startButton.addEventListener("click", () => {
+      const gameContainer = document.querySelector(".game-container");
+      gameContainer.style.display = "block";
+      Game.start();
+      Game.handleClick()
+      
+    });
+  });
+
+  
+  
