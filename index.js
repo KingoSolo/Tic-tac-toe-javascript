8fe5c97589a5ee1f788fc32322f84fdacc11d73e
Adeniran Solomon <soluwaseun806@gmail.com>
2023-07-15 03:00:52 +0100

    design interface

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const displayController = (() => {
+    const renderMessage = (message) =>{
+        document.querySelector("#message").innerHTML = message
+    }
+    return {
+        renderMessage
+      };
+})();
 //setting up the gameboard Module
 let gameBoardModule = (function(){
     let gameBoard = ["","","","","","","","",""];
@@ -51,16 +59,28 @@ const Game = (() => {
 
     const handleClick = (event) =>{
         const cellIndex = Array.from(event.target.parentNode.children).indexOf(event.target);
+        if(gameOver) return;
         if (gameBoardModule.getGameBoard()[cellIndex] !== "") return;
         gameBoardModule.update(cellIndex,players[currentPlayerIndex].mark) 
+
+        if (checkforWin(gameBoardModule.getGameBoard(),players[currentPlayerIndex].mark)){
+            gameOver=true;    
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`);
+        }else if(checkforTie(gameBoardModule.getGameBoard(),players[currentPlayerIndex].mark)){
+            gameOver=true;  
+            displayController.renderMessage("It's a tie!")
+         }
+
         currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
        
       };
-      const restart = () =>{console.log("hi")
+      const restart = () =>{
         for(let i = 0; i<9 ;i++){
             gameBoardModule.update(i,"")
         }
-        gameBoardModule.render()
+        gameBoardModule.render();
+        gameOver = false;
+        document.querySelector("#message").innerHTML = ""
       }
     
     return{
@@ -69,19 +89,44 @@ const Game = (() => {
         handleClick
     };
 })();
-document.addEventListener("DOMContentLoaded", () => {
+
+
+  function checkforWin(gameBoard){
+    const winningCombination = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6]
+    ]
+    for(let i=0; i<winningCombination.length; i++){
+        const [a,b,c] = winningCombination[i]
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] ===gameBoard[c]){
+            return true;
+        }
+    }
+    return false
+    }
+
+   function checkforTie(gameBoard){
+    return gameBoard.every(cell => cell !== "")
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector("#start-button");
+    const restartButton = document.querySelector("#restart-button");
+  
     startButton.addEventListener("click", () => {
       const gameContainer = document.querySelector(".game-container");
       gameContainer.style.display = "block";
-      Game.start(); 
+      Game.start();
+    });
+  
+    restartButton.addEventListener("click", () => {
+      Game.restart();
     });
   });
-
-  const restartBtn = document.querySelector("#new-game")
-  restartBtn.addEventListener("click",()=>{
-    Game.restart();
-  })
-
-  
   
